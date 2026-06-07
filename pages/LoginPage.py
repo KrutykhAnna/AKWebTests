058cbd409857fea15d4d57e9c4137babec1a746b
user <krutykh.anna@icloud.com>
2026-06-07 20:42:48 +0300

    lesson_1: добавлены тесты

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -12,8 +12,38 @@ class LoginPageLocators:
     MAIL_LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,mailru"]')
     YANDEX_LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
 
-    QR_CODE_TAB = (By.XPATH, '//*[@data-l="t,qr_tab"]')
+    QR_TAB = (By.XPATH, '//*[@data-l="t,qr_tab"]')
+    LOGIN_TAB = (By.XPATH, '//*[@data-l="t,login_tab"]')
+
+    ERROR_LOGIN_TEXT = (By.XPATH, '//span[text()="Введите логин"]')
+    ERROR_PASSWORD_TEXT = (By.XPATH, '//span[text()="Введите пароль"]')
 
 
 class LoginPageHelper(BasePage):
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.LOGIN_TAB)
+        self.find_element(LoginPageLocators.QR_TAB)
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_BY_QR_BUTTON)
+        self.find_element(LoginPageLocators.FORGOT_LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.REGISTER_BUTTON)
+        self.find_element(LoginPageLocators.VK_LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.MAIL_LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.YANDEX_LOGIN_BUTTON)
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
+
+    def get_error_login_text(self):
+        return self.find_element(LoginPageLocators.ERROR_LOGIN_TEXT).text
+
+    def get_error_password_text(self):
+        return self.find_element(LoginPageLocators.ERROR_PASSWORD_TEXT).text
+
+    def send_login(self, login: str):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(login)
---
+++ tests/LoginTests.py
@@ -0,0 +1,23 @@
+from faker import Faker
+
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+
+BASE_URL = 'https://ok.ru/'
+EMPTY_LOGIN_ERROR = 'Введите логин'
+EMPTY_PASSWORD_ERROR = 'Введите пароль'
+def test_empty_login_and_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.click_login()
+    assert LoginPage.get_error_login_text() == EMPTY_LOGIN_ERROR, (f"Ожидаемый результат: error_message == {EMPTY_LOGIN_ERROR}, "
+                                                                   f"Фактический результат: error_message == {LoginPage.get_error_login_text()} ")
+
+def test_without_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.send_login(Faker().email())
+    LoginPage.click_login()
+    assert LoginPage.get_error_password_text() == EMPTY_PASSWORD_ERROR,  (f"Ожидаемый результат: error_message == {EMPTY_PASSWORD_ERROR}, "
+                                                                   f"Фактический результат: error_message == {LoginPage.get_error_password_text()} ")
